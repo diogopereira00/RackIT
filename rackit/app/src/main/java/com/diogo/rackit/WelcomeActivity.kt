b60e567ad5000ebfe408a8b127475784bb44a05c
diogopereira00 <83818707+diogopereira00@users.noreply.github.com>
2021-12-10 16:28:14 +0000

    Adicionada AuthenticationActivity
    Botão começar a funcionar(welcomeActivity)

--- rackit/app/src/main/java/com/diogo/rackit/WelcomeActivity.kt
+++ rackit/app/src/main/java/com/diogo/rackit/WelcomeActivity.kt
@@ -1,11 +1,35 @@
 package com.diogo.rackit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.diogo.rackit.databinding.ActivityMainBinding
+import com.diogo.rackit.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+    lateinit var botaoComecar: Button
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        this.binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        botaoComecar = binding.welcomeButton
+        botaoComecar.setOnClickListener{
+            closeOpenActivity(outraActivity = AuthenticationActivity::class.java)
+
+        }
+    }
+
+    private fun executarOutraActivity(outraActivity: Class<*>) {
+        val x = Intent(this, outraActivity)
+        startActivity(x)
+    }
+    private fun closeOpenActivity(outraActivity: Class<*>) {
+        val x = Intent(this, outraActivity)
+        finish()
+        startActivity(x)
     }
 }

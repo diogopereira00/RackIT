6473ac0bb2d9b81281c104851d6fbeaf2d339952
diogopereira00 <83818707+diogopereira00@users.noreply.github.com>
2021-12-10 18:46:46 +0000

    Login UI adicionada

--- rackit/app/src/main/java/com/diogo/rackit/AuthenticationActivity.kt
+++ rackit/app/src/main/java/com/diogo/rackit/AuthenticationActivity.kt
@@ -1,11 +1,30 @@
 package com.diogo.rackit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.diogo.rackit.databinding.ActivityAuthenticationBinding.inflate
+import com.diogo.rackit.databinding.ActivityAuthenticationBinding
 
 class AuthenticationActivity : AppCompatActivity() {
+    lateinit var botaoLogin: Button
+    private lateinit var binding: ActivityAuthenticationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+
+        this.binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        botaoLogin = binding.loginButton
+        botaoLogin.setOnClickListener{
+            executarOutraActivity(outraActivity = LoginActivity::class.java)
+
+        }
     }
-}
+
+    private fun executarOutraActivity(outraActivity: Class<*>) {
+        val x = Intent(this, outraActivity)
+        startActivity(x)
+    }
+    }

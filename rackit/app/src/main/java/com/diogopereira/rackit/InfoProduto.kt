92f53e9f12460a6358851757c8fb3118b9bc8747
diogopereira00 <83818707+diogopereira00@users.noreply.github.com>
2022-01-07 15:09:06 +0000

    infoproduto xml adicionado

---
+++ rackit/app/src/main/java/com/diogopereira/rackit/InfoProduto.kt
@@ -0,0 +1,12 @@
+package com.diogopereira.rackit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diogopereira.rackit.v2.R
+
+class InfoProduto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_produto)
+    }
+}

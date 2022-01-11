bb170e7ab2ba48f084e9f06bce5b090d8f3da853
diogopereira00 <83818707+diogopereira00@users.noreply.github.com>
2022-01-11 22:37:42 +0000

    binding shop fragment

--- rackit/app/src/main/java/com/diogopereira/rackit/fragments/ShoppingListFragment.kt
+++ rackit/app/src/main/java/com/diogopereira/rackit/fragments/ShoppingListFragment.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.diogopereira.rackit.GlobalClass
 import com.diogopereira.rackit.v2.R
+import com.diogopereira.rackit.v2.databinding.FragmentProductsBinding
+import com.diogopereira.rackit.v2.databinding.FragmentShoppingListBinding
 
 
 class ShoppingListFragment : Fragment() {
-
+    private var _binding: FragmentShoppingListBinding? = null
+    private val binding get() = _binding!!
+    lateinit var gv: GlobalClass
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_list, container, false)
+        _binding = FragmentShoppingListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.tag.text ="Teste"
+        return view
     }
 // TODO: 04/01/2022 RECYCLYER VIEW LISTA DE COMPRAS <--------------
 

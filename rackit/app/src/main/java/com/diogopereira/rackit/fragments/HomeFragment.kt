0a67703379b1af39c23c79774eab1ec115c14bed
diogopereira00 <83818707+diogopereira00@users.noreply.github.com>
2022-01-12 17:55:39 +0000

    recyclerviewProdutos +/-

--- rackit/app/src/main/java/com/diogopereira/rackit/fragments/HomeFragment.kt
+++ rackit/app/src/main/java/com/diogopereira/rackit/fragments/HomeFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.diogopereira.rackit.AddProductActivity
 import com.diogopereira.rackit.AuthenticationActivity
 import com.diogopereira.rackit.GlobalClass
+import com.diogopereira.rackit.adapters.HorizontalAdapter
 import com.diogopereira.rackit.v2.databinding.FragmentHomeBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
@@ -38,11 +40,27 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater,container,false)
         val view = binding.root
+        //creating recycler val
+        val recycler = binding.recycler
+
+        //setting recycler to horizontal scroll
+        recycler.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        val data: ArrayList<String> = ArrayList()
+        data.add("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80")
+        data.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg")
+        data.add("https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHw%3D&w=1000&q=80")
+        data.add("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80")
+        data.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg")
+        data.add("https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHw%3D&w=1000&q=80")
+        data.add("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80")
+        data.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg")
+        data.add("https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHw%3D&w=1000&q=80")
+
+        recycler.adapter = HorizontalAdapter(data)
 
         val currentUser = firebaseAuth.currentUser
-        if (currentUser != null) {
             val ref = FirebaseDatabase.getInstance().getReference("Users")
-            ref.child(currentUser.uid)
+            ref.child(currentUser!!.uid)
                 //.addListenerForSingleValueEvent(object : ValueEventListener {  so procura 1x
                 .addValueEventListener(object : ValueEventListener {
 
@@ -51,19 +69,13 @@ class HomeFragment : Fragment() {
                         val name = snapshot.child("name").value.toString()
                         gv.nomeUtilizador = name
                         gv.currentList = "list_"+currentUser.uid
-                        binding.teste.setText(gv.nomeUtilizador)
+                        binding.teste.setText("Olá ${gv.nomeUtilizador}!")
                     }
 
                     override fun onCancelled(error: DatabaseError) {
 
                     }
                 })
-        } else {
-            startActivity(Intent(activity, AuthenticationActivity::class.java))
-
-        }
-
-
 
 
         return view
